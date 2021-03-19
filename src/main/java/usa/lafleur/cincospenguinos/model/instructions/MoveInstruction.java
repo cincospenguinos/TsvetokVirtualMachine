0cf61a5cf5dfa0d4588f9c7a27b4588d6fdbc88f
Andre LaFleur <cincospenguinos@gmail.com>
2021-03-19 17:25:23 -0600

    Handle moving immediate into $ak

--- src/main/java/usa/lafleur/cincospenguinos/model/instructions/MoveInstruction.java
+++ src/main/java/usa/lafleur/cincospenguinos/model/instructions/MoveInstruction.java
@@ -7,9 +7,9 @@ public class MoveInstruction extends Instruction {
 
     public void execute(byte[] memory, byte[] registerArray) {
         if (opFlagSet()) {
-
+            registerArray[0] = getImmediate();
         } else {
-            registerArray[secondRegister()] = registerArray[firstRegister()];
+            registerArray[secondRegisterIndex()] = registerArray[firstRegisterIndex()];
         }
     }
 
@@ -21,11 +21,15 @@ public class MoveInstruction extends Instruction {
         return rawByte == (byte) 0x01;
     }
 
-    private int firstRegister() {
+    private int firstRegisterIndex() {
         return (getRawByte() & 0b00001100) >> 2;
     }
 
-    private int secondRegister() {
+    private int secondRegisterIndex() {
         return (getRawByte() & 0b00000011);
+    }
+
+    private byte getImmediate() {
+        return (byte) (getRawByte() & 0x0f);
     }
 }
--- src/test/java/usa/lafleur/cincospenguinos/MoveInstructionTest.java
+++ src/test/java/usa/lafleur/cincospenguinos/MoveInstructionTest.java
@@ -15,4 +15,13 @@ public class MoveInstructionTest {
         instruction.execute(null, registers);
         assertEquals((byte) 33, registers[1]);
     }
+
+    @Test
+    public void test_executeMovesImmediate() {
+        byte machineInstruction = (byte) 0b10011001; // Move $rej2 into $rej1
+        Instruction instruction = new MoveInstruction(machineInstruction);
+        byte[] registers = new byte[] { 0, 0, 0, 0 };
+        instruction.execute(null, registers);
+        assertEquals((byte) 9, registers[0]);
+    }
 }

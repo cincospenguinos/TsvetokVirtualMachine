bfe72e034a7e5fe7bbffd80fc1a4488c81057733
Andre LaFleur <cincospenguinos@gmail.com>
2021-03-19 18:47:38 -0600

    Extract superclass

--- src/main/java/usa/lafleur/cincospenguinos/model/instructions/AddInstruction.java
+++ src/main/java/usa/lafleur/cincospenguinos/model/instructions/AddInstruction.java
@@ -1,34 +1,12 @@
 package usa.lafleur.cincospenguinos.model.instructions;
 
-import usa.lafleur.cincospenguinos.model.RegisterArray;
-
-public class AddInstruction extends Instruction {
+public class AddInstruction extends LogicArithmeticInstruction {
     public AddInstruction(byte raw) {
         super(raw);
     }
 
     @Override
-    public void execute(byte[] memory, RegisterArray registerArray) {
-        byte first, second;
-
-        if (opFlagSet()) {
-            first = registerArray.accumulator();
-            second = getImmediate();
-        } else {
-            first = registerArray.getRegister(firstRegisterIndex());
-            second = registerArray.getRegister(secondRegisterIndex());
-        }
-
-        int result = (first + second);
-
-        if (result >= 128) {
-            registerArray.setFlag(RegisterArray.OVERFLOW_FLAG);
-        }
-
-        if ((result & 0xff) == 0) {
-            registerArray.setFlag(RegisterArray.ZERO_FLAG);
-        }
-
-        registerArray.setRegister(RegisterArray.ACCUMULATOR_INDEX, (byte) result);
+    protected int performOperation(byte left, byte right) {
+        return left + right;
     }
 }
--- src/main/java/usa/lafleur/cincospenguinos/model/instructions/MultiplyInstruction.java
+++ src/main/java/usa/lafleur/cincospenguinos/model/instructions/MultiplyInstruction.java
@@ -2,24 +2,13 @@ package usa.lafleur.cincospenguinos.model.instructions;
 
 import usa.lafleur.cincospenguinos.model.RegisterArray;
 
-public class MultiplyInstruction extends Instruction {
+public class MultiplyInstruction extends LogicArithmeticInstruction {
     public MultiplyInstruction(byte raw) {
         super(raw);
     }
 
     @Override
-    public void execute(byte[] memory, RegisterArray registerArray) {
-        byte first, second;
-
-        if (opFlagSet()) {
-            first = registerArray.accumulator();
-            second = getImmediate();
-        } else {
-            first = registerArray.getRegister(firstRegisterIndex());
-            second = registerArray.getRegister(secondRegisterIndex());
-        }
-
-        int result = (first * second);
-        registerArray.setRegister(RegisterArray.ACCUMULATOR_INDEX, (byte) result);
+    protected int performOperation(byte left, byte right) {
+        return left * right;
     }
 }

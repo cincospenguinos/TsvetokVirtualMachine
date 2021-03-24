adb1835b1dba8c75d968e1ebf2ec8d00c5403def
Andre LaFleur <cincospenguinos@gmail.com>
2021-03-24 17:03:56 -0600

    Begin class extraction
    - This class extraction will add a seam to get a unit test into place, permitting me to tear down the tests for the assembler and get those into another service
    - The assembler can then simply use other services to assemble an abstract representation of instructions from assembly, and later permit the inclusion of data and code, and aggregate them together into an accepted executable file format

---
+++ src/main/java/usa/lafleur/cincospenguinos/assembler/OpCodeResolutionService.java
@@ -0,0 +1,9 @@
+package usa.lafleur.cincospenguinos.assembler;
+
+public class OpCodeResolutionService {
+
+
+    public byte codeFor(String operation) {
+        return 0;
+    }
+}
--- src/main/java/usa/lafleur/cincospenguinos/assembler/TsvetokAssembler.java
+++ src/main/java/usa/lafleur/cincospenguinos/assembler/TsvetokAssembler.java
@@ -3,14 +3,19 @@ package usa.lafleur.cincospenguinos.assembler;
 import usa.lafleur.cincospenguinos.model.TsvetokInstruction;
 
 public class TsvetokAssembler {
-    public TsvetokAssembler() {}
+    private OpCodeResolutionService opCodeResolver;
+
+    public TsvetokAssembler() {
+        opCodeResolver = new OpCodeResolutionService();
+    }
 
     public TsvetokInstruction createInstruction(String line) {
         String[] pieces = line.trim().split("\\s+");
 
         if (pieces.length == 1) {
             if (pieces[0].equals("noup")) {
-                return new TsvetokInstruction((byte) 0x0, (byte) 0x0);
+                byte opcode = opCodeResolver.codeFor(pieces[0]);
+                return new TsvetokInstruction(opcode, (byte) 0x0);
             }
 
             return new TsvetokInstruction((byte) 0xf0, (byte) 0x0);

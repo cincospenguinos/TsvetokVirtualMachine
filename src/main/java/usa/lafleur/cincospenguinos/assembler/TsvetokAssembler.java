0e4cadd6af62b4f353585b08982f44c9efd966aa
Andre LaFleur <cincospenguinos@gmail.com>
2021-03-24 23:24:50 -0600

    Create helper class to handle machine code assembly
    - I keep forgetting that the assembler does more than just machine code--it'll do a whole lot more. This is why I keep gutting it--by tearing out service classes, I'm abstracting away details so that the assembler doesn't have to worry about random byte stuff

--- src/main/java/usa/lafleur/cincospenguinos/assembler/TsvetokAssembler.java
+++ src/main/java/usa/lafleur/cincospenguinos/assembler/TsvetokAssembler.java
@@ -6,23 +6,68 @@ public class TsvetokAssembler {
     private final OpCodeResolutionService opCodeResolver;
     private final FlagResolutionService flagResolver;
     private final RegisterResolutionService registerResolver;
+    private final InstructionBuilder instructionBuilder;
 
     public TsvetokAssembler() {
         opCodeResolver = new OpCodeResolutionService();
         flagResolver = new FlagResolutionService();
         registerResolver = new RegisterResolutionService();
+        instructionBuilder = new InstructionBuilder();
+    }
+
+    class InstructionBuilder {
+        private byte _opcode;
+        private byte _operationFlags;
+        private byte _leftRegister;
+        private byte _rightRegister;
+
+        InstructionBuilder() {}
+
+        public InstructionBuilder setOpcode(byte opcode) {
+            _opcode = opcode;
+            return this;
+        }
+
+        public InstructionBuilder setOperationFlags(byte flags) {
+            _operationFlags = flags;
+            return this;
+        }
+
+        public InstructionBuilder setLeftRegister(byte lefty) {
+            _leftRegister = lefty;
+            return this;
+        }
+
+        public InstructionBuilder setRightRegister(byte righty) {
+            _rightRegister = righty;
+            return this;
+        }
+
+        public InstructionBuilder clear() {
+            _opcode = 0;
+            _operationFlags = 0;
+            _leftRegister = 0;
+            _rightRegister = 0;
+
+            return this;
+        }
+
+        public TsvetokInstruction build() {
+            byte upper = (byte) ((_opcode << 4) | (_operationFlags));
+            byte lower = (byte) ((_leftRegister << 4) | (_rightRegister));
+
+            return new TsvetokInstruction(upper, lower);
+        }
     }
 
     public TsvetokInstruction createInstruction(String line) {
         String[] pieces = line.trim().split("\\s+");
-        byte flags = flagResolver.flagBitsFor(pieces[0]);
-        byte operationNibble = opCodeResolver.codeFor(pieces[0]);
-        byte opcode = (byte) ((operationNibble << 4) | flags);
-
-        byte leftArg = registerResolver.resolve(pieces[1]);
-        byte rightArg = registerResolver.resolve(pieces[2]);
-        byte arguments = (byte) ((leftArg << 4) | rightArg);
-
-        return new TsvetokInstruction(opcode, arguments);
+        return instructionBuilder
+                .clear()
+                .setOpcode(opCodeResolver.codeFor(pieces[0]))
+                .setOperationFlags(flagResolver.flagBitsFor(pieces[0]))
+                .setLeftRegister(registerResolver.resolve(pieces[1]))
+                .setRightRegister(registerResolver.resolve(pieces[2]))
+                .build();
     }
 }

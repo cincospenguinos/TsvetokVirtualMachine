07e36083faae4f2a7ef3302b77c02bdc35d74626
Andre LaFleur <cincospenguinos@gmail.com>
2021-09-23 18:56:11 -0600

    Tokenizer responds with list of tokens

--- src/main/java/usa/lafleur/cincospenguinos/mini_java/Tokenizer.java
+++ src/main/java/usa/lafleur/cincospenguinos/mini_java/Tokenizer.java
@@ -1,11 +1,29 @@
 package usa.lafleur.cincospenguinos.mini_java;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Tokenizer {
-    private static final String INTEGER_LITERAL_PATTERN = "^-?\\d+$";
+    private final String _program;
 
-    public Tokenizer() {}
+    public Tokenizer(String program) {
+        _program = program;
+    }
 
-    public Token identifierFor(String uninterruptedChunk) {
-        return Token.fromString(uninterruptedChunk);
+    public List<Token> tokenize() {
+        List<Token> tokens = new ArrayList<>();
+        String[] chunkedProgram = _program
+                .replaceAll("\\(", " ( ")
+                .replaceAll("\\)", " ) ")
+                .replaceAll("\\[", " [ ")
+                .replaceAll("]", " ] ")
+                .split("\\s+");
+
+        for (String str: chunkedProgram) {
+            Token token = Token.fromString(str);
+            tokens.add(token);
+        }
+
+        return tokens;
     }
 }
--- src/test/java/usa/lafleur/cincospenguinos/mini_java/TokenizerTest.java
+++ src/test/java/usa/lafleur/cincospenguinos/mini_java/TokenizerTest.java
@@ -1,7 +1,30 @@
 package usa.lafleur.cincospenguinos.mini_java;
 
+import org.junit.Ignore;
 import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static org.junit.Assert.*;
 
-public class TokenizerTest {}
+public class TokenizerTest {
+    @Test
+    public void test_respondsWithListOfTokens() {
+        Token[] correctTokenOrder = {
+            Token.ACCESS_PUBLIC, Token.CLASS, Token.IDENTIFIER, Token.OPEN_BRACE,
+            Token.ACCESS_PUBLIC, Token.STATIC, Token.TYPE_VOID, Token.IDENTIFIER,
+            Token.OPEN_PAREN, Token.IDENTIFIER, Token.OPEN_SQUARE_BRACE, Token.CLOSE_SQUARE_BRACE,
+            Token.IDENTIFIER, Token.CLOSE_PAREN, Token.OPEN_BRACE, Token.CLOSE_BRACE,
+            Token.CLOSE_BRACE,
+        };
+        List<Token> correctTokenList = Arrays.asList(correctTokenOrder);
+
+        Tokenizer tokenizer = new Tokenizer("public class MainClass { \n" +
+                "public static void main(String[] args) {\n" +
+                "}\n" +
+                "}");
+        List<Token> tokenList = tokenizer.tokenize();
+        assertEquals(correctTokenList, tokenList);
+    }
+}

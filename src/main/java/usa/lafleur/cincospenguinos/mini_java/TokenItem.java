2d2419223e10837c7af387585e5fda874536a552
Andre LaFleur <cincospenguinos@gmail.com>
2021-09-23 18:59:56 -0600

    Introduce TokenItem

---
+++ src/main/java/usa/lafleur/cincospenguinos/mini_java/TokenItem.java
@@ -0,0 +1,26 @@
+package usa.lafleur.cincospenguinos.mini_java;
+
+import java.util.Objects;
+
+public class TokenItem {
+    private final Token _token;
+    private final String _value;
+
+    public TokenItem(Token token, String value) {
+        _token = token;
+        _value = value;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TokenItem)) return false;
+        TokenItem tokenItem = (TokenItem) o;
+        return _token == tokenItem._token && _value.equals(tokenItem._value);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(_token, _value);
+    }
+}
---
+++ src/test/java/usa/lafleur/cincospenguinos/mini_java/TokenItemTest.java
@@ -0,0 +1,14 @@
+package usa.lafleur.cincospenguinos.mini_java;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TokenItemTest {
+    @Test
+    public void test_equalityMatchesTokenAndValue() {
+        assertEquals(new TokenItem(Token.OPERATION_ADD, "+"),new TokenItem(Token.OPERATION_ADD, "+"));
+        assertEquals(new TokenItem(Token.IDENTIFIER, "foo"), new TokenItem(Token.IDENTIFIER, "foo"));
+        assertNotEquals(new TokenItem(Token.IDENTIFIER, "foo"), new TokenItem(Token.IDENTIFIER, "bar"));
+    }
+}
